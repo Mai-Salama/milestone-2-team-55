3f1399166ad3f6146f83498a5d7165abcf9a30be
me <maihamdy99@gmail.com>
2021-01-14 02:55:22 +0200

    backend works

--- backend/app.js
+++ backend/app.js
@@ -200,6 +200,24 @@ const authH = (req,res,next)=>{//called it authH to authenticate HR
     }
  }
 
+ const authA = (req,res,next)=>{//called it authA to authenticate an academic member
+    try{
+             const token=req.header('x-auth-token');
+             const JWT_Password="RandomString";
+  
+             const verified = jwt.verify(token,JWT_Password);
+      //       console.log(verified);
+              if(!verified){
+                 return res.status(401).json({msg:"authorization failed"});
+              }
+              req.user=verified.id;
+              next();
+     }
+     catch(error){
+      res.status(500).json({error:error.message});
+     }
+  }
+
 portal.post('/changePasswordHR',authH,async(req,res)=>{
     try{
         const {body} = req;
